0dce7003d04ec90ba5295bf73a9e15570ebd9935
Kyholee <shuttlekyuholee2@gmail.com>
2021-11-10 10:39:25 +0900

    new version

--- src/App.js
+++ src/App.js
@@ -6,27 +6,38 @@ function App() {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
 
+  function handleKeyPress(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+
+      if (count > 19 || count === "Experiment finished. Please reload") {
+        setCount("Experiment finished. Please reload")
+      }else {
+        setCount(count + 1)
+        setName("")
+      }
+
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         
-        <p>You clicked {count} times</p>
+        <p># {count} </p>
 
         <form>
           <label>
-            Name <br/>
-            <input type="text" value={name} onChange={e => setName(e.target.value)} />
+
+            <input 
+              className="App-input"
+              type="text" 
+              value={name} 
+              onChange={e => setName(e.target.value)} 
+              onKeyPress={handleKeyPress}
+            />
+
           </label>
-          <input 
-
-            type="button" 
-            value="Submit" 
-            onClick={e => {
-              setCount(count + 1)
-              setName("")
-            }}
-
-          />
         </form>
 
       </header>
